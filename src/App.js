7d083792a59afb91add5717fb02cf2db15daf69e
Sky Texier <skyhamilton@gmail.com>
2023-01-05 15:11:55 -0800

    Creating components
    Importing some components to test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/Nav/Navbar';
-
+import  Navigation  from './components/Nav';
+import  About  from './components/About';
+import Footer from './components/Footer';
+ 
 function App() {
   return (
     <>
     <div className="App">
-      <NavBar />
-
+      <Navigation />
+      <About />
+      <Footer />
     </div>
     </>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,13 +2,13 @@ import {React, useState, useEffect } from "react";
 
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
-export const NavBar = () => {
+const Navigation = () => {
     
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
     
     return (
-        <NavBar expanded="md" className={scrolled ? "scrolled" : ""}>
+        <Navbar expanded="md" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="/">
                     <h2>
@@ -16,6 +16,8 @@ export const NavBar = () => {
                     </h2>
                 </Navbar.Brand>
             </Container>
-        </NavBar>
+        </Navbar>
     )
 }
+
+export default Navigation;
