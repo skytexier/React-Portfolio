38102f8deff78bcd869d96f786f85a5cd2fb769f
Sky Texier <skyhamilton@gmail.com>
2023-01-10 17:33:15 -0800

    Adding more projects, working on styling
    Working on styling for components and adding more projects

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,4 +1,4 @@
-import { Col } from 'react-bootstrap';
+import  Col  from 'react-bootstrap/Col';
 
  const ProjectCard = ({ title, description, imgURL, URL }) => {
     return (
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,8 +1,11 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import  ProjectCard  from "../ProjectCard";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "../ProjectCard";
 import projectImg1 from "../../assets/FFA.png";
 import projectImg2 from "../../assets/safespace.JPG";
 import projectImg3 from "../../assets/workdb.png";
+import projectImg4 from "../../assets/users.png";
+import projectImg5 from "../../assets/jate.png";
+import projectImg6 from "../../assets/bazaar-buddies.png";
 
 const Projects = () => {
     const projects = [
@@ -25,43 +28,43 @@ const Projects = () => {
             URL: "https://github.com/skytexier/Workforce-Database"
         },
         {
-            title: "The Safe Space",
-            description: "A Twitter/Reddit-esque social media website",
-            imgURL: projectImg2,
-            URL: "https://the-safer-space.herokuapp.com/"
+            title: "Mongo Social Network",
+            description: "The backend of a social media platform built with Mongodb, Express and Node",
+            imgURL: projectImg4,
+            URL: "https://github.com/skytexier/NoSQL-Social-Network-API"
         },
         {
-            title: "The Safe Space",
-            description: "A Twitter/Reddit-esque social media website",
-            imgURL: projectImg2,
-            URL: "https://the-safer-space.herokuapp.com/"
+            title: "PWA Text Editor",
+            description: "A PWA text editor made using JavaScript and IndexedDB",
+            imgURL: projectImg5,
+            URL: "https://github.com/skytexier/PWA-Text-Editor"
         },
         {
-            title: "The Safe Space",
-            description: "A Twitter/Reddit-esque social media website",
-            imgURL: projectImg2,
-            URL: "https://the-safer-space.herokuapp.com/"
+            title: "Bazaar Buddies",
+            description: "An e-commerce platform made using GraphQL, React, MongoDB and Apollo",
+            imgURL: projectImg6,
+            URL: "https://github.com/skytexier/Bazaar-Buddies"
         },
     ];
 
     return (
         <section className="project" id="project">
             <Container>
-                <Row>
+                <Row flex>
                     <Col size={12}>
-                        <h2>Projects</h2>
-                                    <Row>
-                                        {
-                                            projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCard
-                                                    key={index}
-                                                    {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
-                                    </Row>
+                    <h2>Projects</h2>
+                    <Row>
+                        {
+                            projects.map((project, index) => {
+                                return (
+                                    <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                            })
+                        }
+                        </Row>
                     </Col>
                 </Row>
             </Container>
